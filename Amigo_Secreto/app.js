16ab69ea619bcb82eeb5aaadf82317e7163178c5
Nicole <nicole.vivaxfreitas@hotmail.com>
2025-02-02 20:40:23 -0300

    Verificação da função de registrar nome e validar o campo.

--- Amigo_Secreto/app.js
+++ Amigo_Secreto/app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let amigosSorteados = [];
+
+function adicionarAmigo(nome) {
+  document.getElementById('input').value = "";
+
+  if (nome === "") {
+    alert("Por favro, insira um nome.");
+    return;
+  } else {
+    amigos.push(nome);
+  }
+  limparCampo();
+  
+}
+
+
