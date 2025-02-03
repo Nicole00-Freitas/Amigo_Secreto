b6e86264bc11959b42feeb3ab04800de5a48c22e
Nicole <nicole.vivaxfreitas@hotmail.com>
2025-02-02 22:02:44 -0300

    Atualizar a lista de amigos

--- Amigo_Secreto/app.js
+++ Amigo_Secreto/app.js
@@ -1,4 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Adicionar a biblioteca para ler o coteúdo da tela
+// if ('speechSynthesis' in window) {
+//     let utterance = new SpeechSynthesisUtterance(texto);
+//     utterance.lang = 'pt-BR'; 
+//     utterance.rate = 1.2; 
+//     window.speechSynthesis.speak(utterance); 
+// } else {
+//     console.log("Web Speech API não suportada neste navegador.");
+// }
+
+
 let amigos = [];
 
 //Adiciona amigo na lista 
@@ -6,16 +16,25 @@ function adicionarAmigo() {
   let digite = document.getElementById('amigo');
     let nome = digite.value;
 
-    // Verifica se o campo está vazio
+// Verifica se o campo está vazio
   if (nome === "") {
     alert("Por favor, insira um nome.");
     return;
   }
 
-  // Adiciona o nome ao array e limpa o campo de texto
+// Adiciona o nome ao array e limpa o campo de texto
     amigos.push(nome);
     digite.value = "";
 }
 
+//Atualiza a lista de amigos
+function atualizarLista() {
+  let lista = document.getElementById('listaAmigos');
+  lista.innerHTML = "";
+
+  if(amigos === 0) {
+
+  }
+}
 
 
