fa6b8f11ab3d679b4dc20c85f501a394fbf75e24
Nicole <nicole.vivaxfreitas@hotmail.com>
2025-02-02 21:45:09 -0300

    Função para agregar amigos (v.1)

--- Amigo_Secreto/app.js
+++ Amigo_Secreto/app.js
@@ -1,18 +1,21 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
-let amigosSorteados = [];
 
-function adicionarAmigo(nome) {
-  document.getElementById('input').value = "";
+//Adiciona amigo na lista 
+function adicionarAmigo() {
+  let digite = document.getElementById('amigo');
+    let nome = digite.value;
 
+    // Verifica se o campo está vazio
   if (nome === "") {
-    alert("Por favro, insira um nome.");
+    alert("Por favor, insira um nome.");
     return;
-  } else {
-    amigos.push(nome);
   }
-  limparCampo();
-  
+
+  // Adiciona o nome ao array e limpa o campo de texto
+    amigos.push(nome);
+    digite.value = "";
 }
+
 
 
